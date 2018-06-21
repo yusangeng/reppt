5794348222d85313fd9bb8cc519a7528bfc68d2c
yusangeng <yusangeng@outlook.com>
2018-06-22 00:27:14 +0800

    play命令增加自动编译markdown文件的功能

--- src/play/play.js
+++ src/play/play.js
@@ -1,7 +1,22 @@
 import fs from 'fs'
+import path from 'path'
 import utils from '../utils'
+import transpileProjectPlugins from '../build/transpileProjectPlugins'
+import genPPTJs from '../build/genPPTJs'
 
 const { run, log } = utils
+
+async function genPPT (rootPath) {
+  log('正在转译插件脚本...')
+  await transpileProjectPlugins()
+
+  const markdownFilename = path.join(rootPath, 'src/ppt.md')
+  const pptFilename = path.join(rootPath, 'src/ppt.js')
+  const projectPluginsFilename = path.join(rootPath, 'plugins5/index.js')
+
+  log('正在生成PPT脚本...')
+  await genPPTJs(pptFilename, markdownFilename, projectPluginsFilename)
+}
 
 export default async function play () {
   const rootPath = process.cwd()
@@ -11,7 +26,14 @@ export default async function play () {
     throw new Error(`非法目录: ${rootPath}.`)
   }
 
+  await genPPT(rootPath)
+
   log('启动PPT播放, 请访问 http://127.0.0.1:9000')
+
+  fs.watch(path.resolve(rootPath, './src/ppt.md'), async _ => {
+    await genPPT(rootPath)
+  })
+
   return run('npm run webpack-play').then(stdout => {
     log(stdout)
     return Promise.resolve()
